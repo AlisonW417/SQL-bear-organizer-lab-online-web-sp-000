e75a7052786b7d931aa7410e4a1586f16987d140
Alison Williams <apw5011@gmail.com>
2021-01-12 21:00:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "Male", "Black", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7 , "Male", "Male", "Wild", 1);
